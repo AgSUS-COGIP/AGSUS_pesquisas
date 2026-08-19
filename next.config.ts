f1f2ce5324ac1b885a22e2c2237aaa03b56ad8e1
Yassury Sousa Suira <yassury.suira@agenciasus.org.br>
2026-08-19 16:07:54 -0300

    fix: allow ngrok dev origin (#42)
    Permite que o servidor local de desenvolvimento do Next.js seja acessado por meio do túnel do ngrok.
    
    Essa alteração é necessária para testar o SIGAV externamente durante o desenvolvimento e evitar que arquivos estáticos do Next.js sejam bloqueados ao acessar a aplicação pelo domínio do ngrok.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+
+  allowedDevOrigins: [
+    "mucid-precorneal-haleigh.ngrok-free.dev",
+  ],
+
   images: {
     remotePatterns: [
       {
